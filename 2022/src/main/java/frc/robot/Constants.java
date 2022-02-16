6db8dee0c3a10a4ae167b7ef3288472c2a84a09c
Matt <eglanfield@gmail.com>
2022-02-15 21:07:40 -0500

    fixed CAN IDs

--- 2022/src/main/java/frc/robot/Constants.java
+++ 2022/src/main/java/frc/robot/Constants.java
@@ -25,20 +25,20 @@ public final class Constants {
     public static final int DRIVER_CONTROLLER_PORT = 0;
     public static final int OPERATOR_CONTROLLER_PORT = 1;
 
-    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 14;
+    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 1;
     public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 3;
-    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 8;
-    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 9;
+    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 7;
+    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 5;
 
     public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 4;
-    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 1;
-    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 10;
-    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 7;
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 2;
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 8;
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 6;
 
-    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 15;
-    public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 14;
-    public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 16;
-    public static final int DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 17;
+    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 11;
+    public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 12;
+    public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 13;
+    public static final int DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 14;
 
     public static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET = -Math.toRadians(258.39-180.0);
     public static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET = -Math.toRadians(315.09-180.0);
