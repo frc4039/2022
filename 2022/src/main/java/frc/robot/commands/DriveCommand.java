0a7909874b036cefede14ebebd8fece93ae21582
Matt <eglanfield@gmail.com>
2022-03-13 09:08:51 -0400

    added slew rate limiter to drive

--- 2022/src/main/java/frc/robot/commands/DriveCommand.java
+++ 2022/src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.common.math.Vector2;
@@ -12,6 +13,7 @@ public class DriveCommand extends CommandBase {
     private Axis forward;
     private Axis strafe;
     private Axis rotation;
+    private SlewRateLimiter slewFilter = new SlewRateLimiter(3);
 
     public DriveCommand(DrivetrainSubsystem drivetrain, Axis forward, Axis strafe, Axis rotation) {
         this.forward = forward;
@@ -25,7 +27,7 @@ public class DriveCommand extends CommandBase {
 
     @Override
     public void execute() {
-        drivetrainSubsystem.drive(new Vector2(forward.get(true), strafe.get(true)), rotation.get(true) * Constants.kRotationScale, true);
+        drivetrainSubsystem.drive(new Vector2(slewFilter.calculate(forward.get(true)), slewFilter.calculate(strafe.get(true))), slewFilter.calculate(rotation.get(true)) * Constants.kRotationScale, true);
     }
 
     @Override
